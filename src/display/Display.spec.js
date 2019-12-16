a88c3fb22b12bef3a9956e70b28a8a23a67d25ba
Hui <jeffrey.x.hui@gmail.com>
2019-12-16 14:59:29 -0800

    completed display component. MVP achieved!

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,14 +1,72 @@
 // Test away!
 
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, getByText } from '@testing-library/react'
 
 import Display from './Display'
-// test 
+// pass in closed/locked props
+// assert expectations
 
-test('gate defaults unlock', () => {
+test('gate defaults as unlocked and opened', () => {
   const { getByText } = render(<Display />)
 
   getByText(/unlocked/i)
   getByText(/open/i)
 })
+
+describe('gate locking and unlocking', () => {
+  test('gate locked', () => {
+    const { getByText, queryByText } = render(<Display locked={true} />)
+
+    expect(queryByText(/unlocked/i)).toBeNull()
+    getByText(/locked/i)
+  })
+  test('locked gate displays red-led', () => {
+    const { getByText } = render(<Display locked={true} />)
+
+    expect(getByText(/locked/i).className).toBe('led red-led')
+  })
+
+  test('gate unlocked', () => {
+    const { getByText, queryByText } = render(<Display locked={false} />)
+
+    // expect(queryByText(/locked/i)).toBe(/unlocked/i)
+    // problem writing this assertion because unlocked contains the word locked and I want to test to make sure locked isn't present
+    getByText(/unlocked/i)
+  })
+  test('unlocked gate displays green-led', () => {
+    const { getByText } = render(<Display locked={false} />)
+
+    expect(getByText(/unlocked/i).className).toBe('led green-led')
+  })
+})
+
+describe('gate opening and closing', () => {
+  test('gate renders closed', () => {
+    const { getByText, queryByText } = render(<Display closed={true} />)
+
+    getByText(/closed/i)
+    expect(queryByText(/open/i)).toBeNull()
+  })
+
+  test('gate displays red-led when closed', () => {
+    const { getByText } = render(<Display closed={true} />)
+
+    expect(getByText(/closed/i).className).toBe('led red-led')
+  })
+
+  test('gate renders open', () => {
+    const { getByText, queryByText } = render(<Display closed={false} />)
+
+    getByText(/open/i)
+    expect(queryByText(/closed/i)).toBeNull()
+  })
+
+  test('gate displays green-led when open', () => {
+    const { getByText } = render(<Display closed={false} />)
+
+    expect(getByText(/open/i).className).toBe('led green-led')
+  })
+
+})
+
