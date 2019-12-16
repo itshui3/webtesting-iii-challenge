13defc6b0995b4b62103d610c78cdc205fcca58d
Hui <jeffrey.x.hui@gmail.com>
2019-12-16 14:29:00 -0800

    reiterated tests overall under dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,2 +1,16 @@
 // Test away
 
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Dashboard from './Dashboard'
+
+describe('dashboard component', () => {
+
+  it('renders in default unlocked and open', () => {
+    const { getByText } = render(<Dashboard />)
+
+    getByText(/unlocked/i)
+    getByText(/open/i)
+  })
+})
